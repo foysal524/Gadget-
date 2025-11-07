a894afe1c3d9d7c944d5044710e1d86a3bbd45e1
nahinfarhan <nahinfarhan.czs@gmail.com>
2025-11-07 10:21:40 +0600

    Security: Move Firebase credentials to environment variables

--- frontend/src/config/firebase.js
+++ frontend/src/config/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBth9i8gJYyVcr9q_1wflGuebZjQF6ja9I",
-  authDomain: "gadgetbazar-3207a.firebaseapp.com",
-  projectId: "gadgetbazar-3207a",
-  storageBucket: "gadgetbazar-3207a.firebasestorage.app",
-  messagingSenderId: "932693757805",
-  appId: "1:932693757805:web:f1e85c18884164abe2d464"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
